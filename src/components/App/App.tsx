862495502a59d6f88fcb555a0f32a7b60fce0e30
Alexis D <dutaud.alex@gmail.com>
2023-02-20 19:04:16 +0100

    feature: adding react-router dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,21 @@ import { ThemeProvider } from '@emotion/react';
 import { Container, CssBaseline, Paper } from '@mui/material';
 
 import { themeDark } from '../theme/theme';
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme={themeDark}>
       <CssBaseline />
       <Container>
-        <Paper elevation={1}>{/* composants here */}</Paper>
+        <Paper elevation={1}>
+          {/* <Routes>
+            <Route
+              path="/"
+              element={}
+            />
+          </Routes> */}
+        </Paper>
       </Container>
     </ThemeProvider>
   );

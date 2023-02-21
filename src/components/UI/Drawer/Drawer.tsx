03d3ccbff8bcad729cb9b0cff5537277fddf7c68
Alexis D <dutaud.alex@gmail.com>
2023-02-21 17:15:00 +0100

    conditionnal rendering drawer

--- src/components/UI/Drawer/Drawer.tsx
+++ src/components/UI/Drawer/Drawer.tsx
@@ -5,18 +5,22 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { selectDrawerState, toggleDrawer } from '../../../features/UI/uiSlice';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import DrawerList from './DrawerList/DrawerListItem/DrawerList';
-
+import { selectIsLogged } from '../../../features/user/userSlice';
 export default function Drawer(): JSX.Element {
+  const isLogged = useAppSelector(selectIsLogged);
   const isDrawerOpen = useAppSelector(selectDrawerState);
   const dispatch = useAppDispatch();
   return (
     <>
-      <IconButton
-        onClick={() => dispatch(toggleDrawer())}
-        sx={{ position: 'absolute', right: '0.1', top: '50%' }}
-      >
-        <ArrowForwardIosIcon />
-      </IconButton>
+      {isLogged && (
+        <IconButton
+          onClick={() => dispatch(toggleDrawer())}
+          sx={{ position: 'absolute', right: '0.1', top: '50%' }}
+        >
+          <ArrowForwardIosIcon />
+        </IconButton>
+      )}
+
       <SwipeableDrawer
         sx={{ position: 'relative' }}
         open={isDrawerOpen}

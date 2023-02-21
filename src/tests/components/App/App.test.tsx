b382555990b26bf5d02bc4b7e25e66e0ae393c46
Alexis D <dutaud.alex@gmail.com>
2023-02-21 15:06:37 +0100

    fix: unit test fixed

--- src/tests/components/App/App.test.tsx
+++ src/tests/components/App/App.test.tsx
@@ -3,23 +3,37 @@ import { render, screen } from '@testing-library/react';
 import App from '../../../components/App/App';
 import { Provider } from 'react-redux';
 import { store } from '../../../redux/store';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from '@remix-run/router';
 
 describe('App components test suites', () => {
   it('should render nav', () => {
+    const history = createMemoryHistory();
     render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Router
+        location={history.location}
+        navigator={history}
+      >
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
     );
     const nav = screen.getByText('HMD');
     expect(nav).toBeInTheDocument();
   });
 
   it('should render footer', () => {
+    const history = createMemoryHistory();
     render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Router
+        location={history.location}
+        navigator={history}
+      >
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
     );
     const footer = screen.getByText('Nos réseaux');
     expect(footer).toBeInTheDocument();

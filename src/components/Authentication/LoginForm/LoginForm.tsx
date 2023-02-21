3ce0abf66f84704cc00c65334ebf0bd59ee25338
AfraAtls <afra.aitlasri@gmail.com>
2023-02-21 12:15:36 +0100

    feat: connect LoginForm to reducer => controlled from inputs

--- src/components/Authentication/LoginForm/LoginForm.tsx
+++ src/components/Authentication/LoginForm/LoginForm.tsx
@@ -1,14 +1,25 @@
-import {Button, FormControl, FormGroup, Paper, TextField, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import { Button, FormControl, FormGroup, Paper, TextField, Typography } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 
+import { setEmail, setPassword } from "../../../features/login/loginSlice";
+
 const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const email = useAppSelector(state => state.login.email);
+  const password = useAppSelector(state => state.login.password);
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setEmail(event.target.value));
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setPassword(event.target.value));
+  };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    // envoyer au backend
+    // envoyer au backend ?
+    console.log(email, password)
   };
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
-  }
     return (
       <Paper sx={{margin: '0 auto', width:"100%" }}>
         <form onSubmit={handleSubmit}>
@@ -23,16 +34,16 @@ const LoginForm = () => {
               placeholder="entrez votre email"
               variant="standard"
               type="email"
-              value=""
-              onChange={handleChange}
+              value={email}
+              onChange={handleEmailChange}
             />
               <TextField
               sx={{py:2}}
               label="password"
               variant="standard"
               type="password"
-              value=""
-              onChange={handleChange}
+              value={password}
+              onChange={handlePasswordChange}
             />
             </FormGroup>
             <Button type="submit" variant="contained" endIcon={<SendIcon />}>

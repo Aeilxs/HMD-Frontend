e5f187f311d7fb78c36d6b45621fdcbd5090cccd
AfraAtls <afra.aitlasri@gmail.com>
2023-02-21 11:46:56 +0100

    feat: add reducer for loginForm

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import uiReducer from '../features/UI/uiSlice';
+import loginReducer from '../features/login/loginSlice';
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
+    login : loginReducer,
   },
 });
 

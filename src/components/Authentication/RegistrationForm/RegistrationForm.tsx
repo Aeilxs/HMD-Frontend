ee73ea0d209382870b7fcd82e5c54dcec27aa443
AfraAtls <afra.aitlasri@gmail.com>
2023-02-21 12:19:30 +0100

    feat: connect RegistrationForm to reducer registrationSlice => controlled form inputs

--- src/components/Authentication/RegistrationForm/RegistrationForm.tsx
+++ src/components/Authentication/RegistrationForm/RegistrationForm.tsx
@@ -1,13 +1,35 @@
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { Button, FormControl, FormControlLabel, FormGroup, FormLabel, Paper, Radio, RadioGroup, TextField, Typography } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import { setFirstname, setLastname, setEmail, setPassword, setGender } from "../../../features/registration/registrationSlice";
 
 const RegistrationForm = () =>  {
+  const dispatch = useAppDispatch();
+  const firstname = useAppSelector(state => state.registration.firstname);
+  const lastname = useAppSelector(state => state.registration.lastname);
+  const email = useAppSelector(state => state.registration.email);
+  const password = useAppSelector(state => state.registration.password);
+  const gender = useAppSelector(state => state.registration.gender);
+
+  const handleFirstnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setFirstname(event.target.value));
+  };
+  const handleLastnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setLastname(event.target.value));
+  };
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setEmail(event.target.value));
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setPassword(event.target.value));
+  };
+  const handleGenderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if(event.target.value === "Femme" || event.target.value === "Homme")
+      dispatch(setGender(event.target.value))
+  };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
   };
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
-  }
   return (
     <Paper sx={{margin: '0 auto', width:"100%" }}>
       <form onSubmit={handleSubmit}>
@@ -22,8 +44,8 @@ const RegistrationForm = () =>  {
               placeholder="entrez votre prénom"
               variant="standard"
               type="text"
-              value="Test"
-              onChange={handleChange}
+              value={firstname}
+              onChange={handleFirstnameChange}
             />
               <TextField
               sx={{py:2}}
@@ -31,8 +53,8 @@ const RegistrationForm = () =>  {
               placeholder="entrez votre nom"
               variant="standard"
               type="text"
-              value="Nom"
-              onChange={handleChange}
+              value={lastname}
+              onChange={handleLastnameChange}
             />
             <TextField
               sx={{py:2}}
@@ -40,8 +62,8 @@ const RegistrationForm = () =>  {
               placeholder="entrez votre email"
               variant="standard"
               type="email"
-              value="test@oclock.io"
-              onChange={handleChange}
+              value={email}
+              onChange={handleEmailChange}
             />
             <TextField
               sx={{py:2}}
@@ -49,16 +71,16 @@ const RegistrationForm = () =>  {
               placeholder=""
               variant="standard"
               type="password"
-              value=""
-              onChange={handleChange}
+              value={password}
+              onChange={handlePasswordChange}
             />
             <FormControl>
             <FormLabel id="demo-radio-buttons-group-label" sx={{textAlign:"start"}}>Gender</FormLabel>
             <RadioGroup
               row
               aria-labelledby="demo-radio-buttons-group-label"
-              value="Femme"
-              onChange={handleChange}
+              value={gender}
+              onChange={handleGenderChange}
               name="radio-buttons-group"
             >
               <FormControlLabel value="Femme" control={<Radio />} label="Femme" />

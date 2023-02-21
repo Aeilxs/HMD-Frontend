203f7b4863df4dc45a906febe34eeaadecc6ce7e
Alexis D <dutaud.alex@gmail.com>
2023-02-21 10:26:34 +0100

    feature: home page link, navigate to home page in nav element

--- src/components/UI/Nav/Nav.tsx
+++ src/components/UI/Nav/Nav.tsx
@@ -10,36 +10,51 @@ import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { selectTheme, toggleTheme } from '../../../features/UI/uiSlice';
 import { DarkMode } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav(): JSX.Element {
   const isDark = useAppSelector(selectTheme);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ height: '10vh' }}>
       <AppBar sx={{ borderBottom: '1px solid grey' }}>
         <Toolbar>
-          <MonitorHeartIcon
-            fontSize="large"
-            sx={{ mr: 2 }}
-          />
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '0.5rem' }}
+          <Box
+            onClick={() => navigate('/')}
+            sx={{ display: 'flex', cursor: 'pointer' }}
           >
-            HMD
-          </Typography>
-          <Button
-            sx={{ mr: 2 }}
-            color="inherit"
-            variant="outlined"
-          >
-            LOGOUT
-          </Button>
-          <Avatar sx={{ mr: 1 }}>A</Avatar>
-          <IconButton onClick={() => dispatch(toggleTheme())}>
-            {isDark ? <LightModeIcon /> : <DarkMode />}
-          </IconButton>
+            <MonitorHeartIcon
+              fontSize="large"
+              sx={{ mr: 2 }}
+            />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '0.5rem',
+              }}
+            >
+              HMD
+            </Typography>
+          </Box>
+          <Box sx={{ ml: 'auto', display: 'flex' }}>
+            <Button
+              sx={{ mr: 2 }}
+              color="inherit"
+              variant="outlined"
+            >
+              LOGOUT
+            </Button>
+            <Avatar sx={{ mr: 1 }}>A</Avatar>
+            <IconButton onClick={() => dispatch(toggleTheme())}>
+              {isDark ? <LightModeIcon /> : <DarkMode />}
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>

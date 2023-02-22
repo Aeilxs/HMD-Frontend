15b2f831640e3319988812a295396881b9da96ca
AfraAtls <afra.aitlasri@gmail.com>
2023-02-22 23:32:56 +0100

    feat: integration of static component drugPage

--- src/components/views/DrugPage/DrugPage.tsx
+++ src/components/views/DrugPage/DrugPage.tsx
@@ -1,3 +1,18 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+import MessageBox from "../../UI/MessageBox/MessageBox";
+
 export default function DrugPage(): JSX.Element {
-  return <div>drug page</div>;
+  return (
+    <Container>
+      <MessageBox title="En savoir plus" content="lorem lorem lorem lorem lorem lorem lorem" width={100}/>
+      <Box sx={{display:'flex', flexDirection:'column', width: '50%', mx: 'auto', my:2}}>
+        <TextField label="Quel medicament prenez-vous ?" variant="standard" sx={{py:3}}/>
+        <TextField label="Dose" variant="standard" sx={{py:3}}/>
+        <TextField label="Informations complementaires" variant="standard" sx={{py:3}}/>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: '2em' }}>
+          <Button variant="contained">Envoyer</Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }

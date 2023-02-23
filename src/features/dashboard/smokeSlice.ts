5b0e41a914571308b9b4679b6de27bfa0ef3a023
Alexis D <dutaud.alex@gmail.com>
2023-02-24 00:35:53 +0100

    feat: smokePage integration, smokePage reducer

--- src/features/dashboard/smokeSlice.ts
+++ src/features/dashboard/smokeSlice.ts
@@ -1,22 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../redux/store';
 
-export interface smokeState {
-
+export interface SmokeState {
+  date: string | null;
+  quantity: number | '';
 }
 
-const initialState: smokeState = {
-
+const initialState: SmokeState = {
+  date: null,
+  quantity: '',
 };
 
 export const smokeSlice = createSlice({
   name: 'smoke',
   initialState,
   reducers: {
+    setSmokeDate: (state, action: PayloadAction<string>) => {
+      return { ...state, date: action.payload };
+    },
+    setSmokeQuantity: (state, action: PayloadAction<number>) => {
+      return { ...state, quantity: action.payload };
+    },
   },
 });
 
-export const {  } = smokeSlice.actions;
+export const { setSmokeDate, setSmokeQuantity } = smokeSlice.actions;
 
+export const selectSmokeDate = (state: RootState) => state.smoke.date;
+export const selectSmokeQuantity = (state: RootState) => state.smoke.quantity;
 
 export default smokeSlice.reducer;

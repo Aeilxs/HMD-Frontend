d2915dd566e81b2dc28b81cda78100b9a05749a5
Alexis D <dutaud.alex@gmail.com>
2023-02-23 17:19:44 +0100

    feat: profilePage integration finished

--- src/components/views/ProfilePage/ProfilePage.tsx
+++ src/components/views/ProfilePage/ProfilePage.tsx
@@ -1,55 +1,68 @@
 import { Button, Container, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { selectDateOfBirth, setDateOfBirth } from '../../../features/user/userSlice';
-import { useAppSelector } from '../../../redux/hooks';
+import {
+  selectDateOfBirth,
+  selectHeight,
+  selectWeight,
+  setDateOfBirth,
+  setHeight,
+  setWeight,
+} from '../../../features/user/userSlice';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import CustomDatePicker from '../../UI/CustomDatePicker/CustomDatePicker';
 import MessageBox from '../../UI/MessageBox/MessageBox';
 
 export default function ProfilePage(): JSX.Element {
+  const dispatch = useAppDispatch();
   const dateOfBirth = useAppSelector(selectDateOfBirth);
+  const weight = useAppSelector(selectWeight);
+  const height = useAppSelector(selectHeight);
   return (
-    <Container>
-      <Box sx={{ mt: 2, display: 'flex', flexDirection: 'column' }}>
-        <Typography
-          sx={{ mb: 2 }}
-          variant="h3"
+    <Container sx={{ mt: 2 }}>
+      <MessageBox
+        title="Pourquoi avons nous besoin de ces informations ?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        width={100}
+      />
+      <Typography
+        sx={{ mt: 2, mb: 2 }}
+        variant="h3"
+        align="center"
+      >
+        A propos de vous
+      </Typography>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column' }}
+        component="form"
+      >
+        <CustomDatePicker
+          value={dateOfBirth}
+          actionCreator={setDateOfBirth}
+        />
+        <TextField
+          onChange={(event) => dispatch(setWeight(Number(event.target.value)))}
+          value={weight}
+          id="outlined-basic"
+          label="Poids (kg)"
+          type="number"
+          variant="outlined"
+          sx={{ mb: 1, mt: 1 }}
+        />
+        <TextField
+          onChange={(event) => dispatch(setHeight(Number(event.target.value)))}
+          value={height}
+          id="outlined-basic"
+          label="Taille (cm)"
+          type="number"
+          variant="outlined"
+          sx={{ mb: 1 }}
+        />
+        <Button
+          sx={{ m: 'auto' }}
+          variant="contained"
         >
-          A propos de vous
-        </Typography>
-        <Box sx={{ display: 'flex', flexDirection: ['column', 'row'] }}>
-          <Box
-            component="form"
-            sx={{ width: '50%', display: 'flex', flexDirection: 'column', mb: 2, mr: 2 }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="Taille (cm)"
-              type="number"
-              variant="outlined"
-              sx={{ mb: 1 }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Poids (kg)"
-              variant="outlined"
-              sx={{ mb: 1 }}
-            />
-            <CustomDatePicker
-              value={dateOfBirth}
-              actionCreator={setDateOfBirth}
-            />
-            <Box sx={{ m: 'auto', mt: 2 }}>
-              <Button variant="contained">Envoyer</Button>
-            </Box>
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <MessageBox
-              title="Pourquoi avons nous besoin de ces informations ?"
-              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              width={100}
-            />
-          </Box>
-        </Box>
+          Envoyer
+        </Button>
       </Box>
     </Container>
   );
--- src/tests/features/userSlice.test.ts
+++ src/tests/features/userSlice.test.ts
@@ -4,12 +4,18 @@ describe('user reducer test suite', () => {
   const initialState: UserState = {
     isLogged: false,
     dateOfBirth: '',
+    age: 0,
+    height: 0,
+    weight: 0,
   };
 
   it('should handle initial state', () => {
     expect(userReducer(initialState, { type: 'unknown' })).toEqual({
       isLogged: false,
       dateOfBirth: '',
+      age: 0,
+      height: 0,
+      weight: 0,
     });
   });
 });

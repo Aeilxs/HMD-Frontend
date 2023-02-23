a7772fae5bc9d0495a9c0c83d908d4a904715271
AfraAtls <afra.aitlasri@gmail.com>
2023-02-23 11:27:33 +0100

    Start integration of static component SportPage

--- src/components/views/SportPage/SportPage.tsx
+++ src/components/views/SportPage/SportPage.tsx
@@ -1,3 +1,60 @@
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Checkbox,
+  FormLabel,
+} from '@mui/material';
+import MessageBox from '../../UI/MessageBox/MessageBox';
+
 export default function SportPage(): JSX.Element {
-  return <div>sport page</div>;
+  const activities = ['course', 'marche', 'natation', 'velo', 'exercices'];
+  return (
+    <Container>
+      <MessageBox
+        title="En savoir plus"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mauris sit amet velit tristique pretium ut sed eros. Sed vel efficitur mauris. Sed euismod aliquam libero id convallis."
+        width={100}
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '50%', mx: 'auto', my: 2 }}>
+        <FormGroup sx={{ display: 'flex', flexDirection: 'column' }}>
+          <FormLabel component="legend">Activités</FormLabel>
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            {activities.map((activity) => (
+              <FormControlLabel
+                key={activity}
+                control={<Checkbox />}
+                label={activity}
+                sx={{ p: 0.2 }}
+              />
+            ))}
+          </Box>
+        </FormGroup>
+        <TextField
+          type="number"
+          label="Temps de l'activité"
+          variant="standard"
+          sx={{ py: 3, mt: 3 }}
+        />
+        <FormControl sx={{ mt: 3 }}>
+          <InputLabel>Intensité</InputLabel>
+          <Select label="Intensité">
+            <MenuItem value={10}>Faible</MenuItem>
+            <MenuItem value={20}>Modérée</MenuItem>
+            <MenuItem value={30}>Élevée</MenuItem>
+          </Select>
+        </FormControl>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: '2em' }}>
+          <Button variant="contained">Envoyer</Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }

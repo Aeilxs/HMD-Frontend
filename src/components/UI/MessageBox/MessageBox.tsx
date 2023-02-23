710393fdf46e58c9d8f7125557494c66a440b684
Alexis D <dutaud.alex@gmail.com>
2023-02-23 14:43:32 +0100

    wip: reponsive

--- src/components/UI/MessageBox/MessageBox.tsx
+++ src/components/UI/MessageBox/MessageBox.tsx
@@ -3,32 +3,31 @@ import Typography from '@mui/material/Typography';
 import { ExpandMore, Info } from '@mui/icons-material';
 
 interface MessageProps {
-    title:string;
-    content:string;
-    width: number;
+  title: string;
+  content: string;
+  width: number;
 }
 
-function MessageBox({title, content, width}: MessageProps) {
-return (
-
-      <Accordion sx={{ width: `${width}%`}}>
-        <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel1a-content"
-            >
-            <Typography variant='h3' sx={{fontSize:"2em", p:1}}>
-                <Info sx={{pr:1}} />
-                {title}
-            </Typography>
-            </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            {content}
-          </Typography>
-        </AccordionDetails>
+function MessageBox({ title, content, width }: MessageProps) {
+  return (
+    <Accordion sx={{ width: `${width}%` }}>
+      <AccordionSummary
+        expandIcon={<ExpandMore />}
+        aria-controls="panel1a-content"
+      >
+        <Typography
+          variant="h3"
+          sx={{ fontSize: '2em', p: 1 }}
+        >
+          <Info sx={{ pr: 1 }} />
+          {title}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>{content}</Typography>
+      </AccordionDetails>
     </Accordion>
-
-  )
+  );
 }
 
 export default MessageBox;
--- src/components/views/ProfilePage/ProfilePage.tsx
+++ src/components/views/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,47 @@
+import { Container, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import MessageBox from '../../UI/MessageBox/MessageBox';
+
 export default function ProfilePage(): JSX.Element {
-  return <div>profile page</div>;
+  return (
+    <Container>
+      <Box sx={{ mt: 2, display: 'flex', flexDirection: 'column' }}>
+        <Typography
+          sx={{ mb: 2 }}
+          variant="h3"
+        >
+          A propos de vous
+        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: ['column', 'row'] }}>
+          <Box
+            component="form"
+            sx={{ width: '50%', display: 'flex', flexDirection: 'column', mb: 2, mr: 2 }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Taille (cm)"
+              type="number"
+              variant="outlined"
+              sx={{ mb: 1 }}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Poids (kg)"
+              variant="outlined"
+              sx={{ mb: 1 }}
+            />
+            {/* Remplacer par custom Date picker */}
+            <TextField label="replaceme" />
+          </Box>
+          <Box sx={{ width: '50%' }}>
+            <MessageBox
+              title="Pourquoi avons nous besoin de ces informations ?"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              width={100}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
 }

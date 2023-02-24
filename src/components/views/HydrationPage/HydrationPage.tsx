03f642337c0fc8e144973b079a3168832a2677fc
Alexis D <dutaud.alex@gmail.com>
2023-02-24 14:42:29 +0100

    feat: hydrationPage integration

--- src/components/views/HydrationPage/HydrationPage.tsx
+++ src/components/views/HydrationPage/HydrationPage.tsx
@@ -1,3 +1,49 @@
+import { Button, TextField } from '@mui/material';
+import { Box, Container } from '@mui/system';
+import {
+  selectHydrationDate,
+  setDate,
+  setQuantity,
+  selectHydrationQuantity,
+} from '../../../features/dashboard/hydrationSlice';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import CustomDatePicker from '../../UI/CustomDatePicker/CustomDatePicker';
+import MessageBox from '../../UI/MessageBox/MessageBox';
+
 export default function HydrationPage(): JSX.Element {
-  return <div>hydration page</div>;
+  const dispatch = useAppDispatch();
+  const date = useAppSelector(selectHydrationDate);
+  const quantity = useAppSelector(selectHydrationQuantity);
+  return (
+    <Container sx={{ mt: 2 }}>
+      <MessageBox
+        title="Pourquoi s'hydrater ?"
+        content="L'eau contenue dans le cerveau (85%) lui permet de mieux fonctionner. Un état de déshydratation peut de ce fait impacter les fonctions cognitives et l'humeur. Ces changements d'humeur se caractérisent par un état de fatigue, de colère, de tension, de perte de mémoire ou de dépression. Ainsi, pour avoir un cerveau fonctionnant correctement et être de bonne humeur, il faut boire beaucoup d'eau."
+        width={100}
+      />
+      <Box
+        component="form"
+        sx={{ mt: 2, display: 'flex', flexDirection: 'column' }}
+      >
+        <CustomDatePicker
+          value={date}
+          actionCreator={setDate}
+        />
+        <TextField
+          onChange={(event) => dispatch(setQuantity(Number(event.target.value)))}
+          value={quantity}
+          label="Quantité (litre(s))"
+          type="number"
+          variant="outlined"
+          sx={{ mb: 1, mt: 1 }}
+        />
+        <Button
+          sx={{ m: 'auto' }}
+          variant="contained"
+        >
+          Valider
+        </Button>
+      </Box>
+    </Container>
+  );
 }
--- src/features/dashboard/hydrationSlice.ts
+++ src/features/dashboard/hydrationSlice.ts
@@ -1,22 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../redux/store';
 
-export interface hydrationState {
-
+export interface HydrationState {
+  date: string | null;
+  quantity: number | '';
 }
 
-const initialState: hydrationState = {
-
+const initialState: HydrationState = {
+  date: null,
+  quantity: '',
 };
 
 export const hydrationSlice = createSlice({
   name: 'hydration',
   initialState,
   reducers: {
+    setDate: (state, action: PayloadAction<string>) => {
+      return { ...state, date: action.payload };
+    },
+    setQuantity: (state, action: PayloadAction<number>) => {
+      return { ...state, quantity: action.payload };
+    },
   },
 });
 
-export const {  } = hydrationSlice.actions;
+export const { setDate, setQuantity } = hydrationSlice.actions;
 
+export const selectHydrationDate = (state: RootState) => state.hydration.date;
+export const selectHydrationQuantity = (state: RootState) => state.hydration.quantity;
 
 export default hydrationSlice.reducer;

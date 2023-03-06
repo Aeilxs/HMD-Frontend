8297d881eaab2a6ae28792ab40293ac151006301
Alexis D <dutaud.alex@gmail.com>
2023-03-06 14:52:45 +0100

    fix: redirect to dashboard after logging isn't breaking the dashboard anymore

--- src/utils/chartsData.ts
+++ src/utils/chartsData.ts
@@ -1,8 +1,11 @@
+import { dataHydrationApi } from '../reducers/dashboard/hydration/hydrationSlice';
+import { dataSleepApi } from '../reducers/dashboard/sleep/sleepSlice';
+import { dataSmokeApi } from '../reducers/dashboard/smoke/smokeSlice';
+import { dataSportApi } from '../reducers/dashboard/sport/sportSlice';
 import { store } from '../store/store';
 import { calcDate } from './math';
 
-export const sleepChartData = () => {
-  const sleeps = store.getState().user.sleeps;
+export const sleepChartData = (sleeps: dataSleepApi[]) => {
   const dates: string[] = [];
   const amounts: number[] = [];
   const quality: number[] = [];
@@ -14,8 +17,7 @@ export const sleepChartData = () => {
   return { sleepDates: dates, sleepAmounts: amounts, sleepQualities: quality };
 };
 
-export const smokeChartData = () => {
-  const smokes = store.getState().user.cigarettes;
+export const smokeChartData = (smokes: dataSmokeApi[]) => {
   const dates: string[] = [];
   const amounts: number[] = [];
   smokes.forEach((smoke) => {
@@ -25,8 +27,7 @@ export const smokeChartData = () => {
   return { smokeDates: dates, smokeAmounts: amounts };
 };
 
-export const hydrationsChartData = () => {
-  const hydrations = store.getState().user.hydratations;
+export const hydrationsChartData = (hydrations: dataHydrationApi[]) => {
   const dates: string[] = [];
   const amounts: number[] = [];
   hydrations.forEach((hydration) => {
@@ -45,8 +46,7 @@ interface ActivityData {
   [key: string]: number;
 }
 
-export const activitiesChartData = () => {
-  const activities = store.getState().user.activities;
+export const activitiesChartData = (activities: dataSportApi[]) => {
   if (activities.length === 0) return { activitiesLabels: false, activitiesPercentage: false };
   const labels: string[] = ['Marche', 'Footing', 'Natation', 'Velo', 'Autre'];
   const data: ActivityData = activities.reduce(
--- src/views/DashboardPage/DashboardPage.tsx
+++ src/views/DashboardPage/DashboardPage.tsx
@@ -5,18 +5,15 @@ import SmokesGraph from './SmokesGraph/SmokesGraph';
 import SleepGraph from './SleepsGraph/SleepsGraph';
 import DrugsGraph from './DrugsGraph/DrugsGraph';
 import FoodGraph from './FoodGraph/FoodGraph';
-import {
-  sleepChartData,
-  smokeChartData,
-  hydrationsChartData,
-  activitiesChartData,
-} from '../../utils/chartsData';
+import { sleepChartData, smokeChartData, hydrationsChartData, activitiesChartData } from '../../utils/chartsData';
+import { useAppSelector } from '../../store/hooks';
+import { selectHydrations, selectSleeps, selectSmokes, selectSports } from '../../reducers/user/userSlice';
 
 export default function DashboardPage(): JSX.Element {
-  const { smokeDates, smokeAmounts } = smokeChartData();
-  const { sleepDates, sleepQualities, sleepAmounts } = sleepChartData();
-  const { hydrationsDates, hydrationAmounts } = hydrationsChartData();
-  const { activitiesPercentages, activitiesLabels } = activitiesChartData();
+  const { smokeDates, smokeAmounts } = smokeChartData(useAppSelector(selectSmokes));
+  const { sleepDates, sleepQualities, sleepAmounts } = sleepChartData(useAppSelector(selectSleeps));
+  const { hydrationsDates, hydrationAmounts } = hydrationsChartData(useAppSelector(selectHydrations));
+  const { activitiesPercentages, activitiesLabels } = activitiesChartData(useAppSelector(selectSports));
 
   return (
     <Grid>

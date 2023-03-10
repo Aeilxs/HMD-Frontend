aa7f3ab6d21538b9a10757cc3badc7c347a7d51c
AfraAtls <afra.aitlasri@gmail.com>
2023-03-10 13:55:57 +0100

    data dynamic for food graph

--- src/views/DashboardPage/FoodGraph/FoodGraph.tsx
+++ src/views/DashboardPage/FoodGraph/FoodGraph.tsx
@@ -1,11 +1,25 @@
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { useResize } from '../../../hooks/useResize';
 import { Paper } from '@mui/material';
 import { useState } from 'react';
 import { Box } from '@mui/system';
 
-export default function FoodGraphs(): JSX.Element {
+interface FoodGraphProps {
+  dates: string[];
+  foodIntakes: number[];
+  foodNeeds: number[];
+}
+
+export default function FoodGraphs({ dates, foodIntakes, foodNeeds }: FoodGraphProps): JSX.Element {
   const vwValue = useResize();
   const [elevation, setElevation] = useState(2);
   ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
@@ -22,18 +36,18 @@ export default function FoodGraphs(): JSX.Element {
     },
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = dates;
   const data = {
     labels,
     datasets: [
       {
         label: 'Calories consommées',
-        data: [1800, 1900, 1700, 1500, 2500, 2300, 2400, 2000, 2000, 2100],
+        data: foodIntakes,
         backgroundColor: '#f79829',
       },
       {
         label: 'Apport journalier recommandé',
-        data: [2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000],
+        data: foodNeeds,
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
     ],
@@ -45,7 +59,14 @@ export default function FoodGraphs(): JSX.Element {
       onMouseEnter={() => setElevation(8)}
       onMouseLeave={() => setElevation(2)}
       elevation={elevation}
-      sx={{ p: 2, gridColumn: '1 / span 2', height: '100%', minHeight: '250px', textAlign: 'right', cursor: 'pointer' }}
+      sx={{
+        p: 2,
+        gridColumn: '1 / span 2',
+        height: '100%',
+        minHeight: '250px',
+        textAlign: 'right',
+        cursor: 'pointer',
+      }}
     >
       <Box
         component={Bar}
